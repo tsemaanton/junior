67a2cb48096909e932fc696050890b7f3aac7996
tsemaanton <alsinghspb@gmail.com>
2017-10-19 17:55:59 +0300

    изм

---
+++ chapter_001/src/main/java/ru/job4j/calculator/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculator task.
+*
+* @author Anton Tsema (mailto:alsinghspb@gmail)
+* @version 0.1
+* @since 18/10/17
+*/
+package ru.job4j;
---
+++ chapter_001/src/test/java/ru/job4j/max/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for MaxTest.
+*
+* @author Anton Tsema (mailto:alsinghspb@gmail)
+* @version 0.1
+* @since 19.10.2017
+*/
+package ru.job4j.max;
