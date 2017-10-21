f9bf73656337aef8021dc3993096b18942c31890
tsemaanton <alsinghspb@gmail.com>
2017-10-22 00:22:56 +0300

    Task 190

--- chapter_001/src/main/java/ru/job4j/max/Max.java
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -12,4 +12,22 @@ public class Max {
     public int max(int first, int second) {
 		return (first > second ? first : second);
  }
+/**
+максимальное значение из 3х чисел.
+@param first first
+@param second second
+@param third third
+@return наибольшее
+*/
+public int max(int first, int second, int third) {
+	while (max(first, second) > max(second, third)) {
+		return first;
+	}
+	while (max(second, third) > max(first, third)) {
+	return second;
+	}
+	while (max(third, first) > max(third, second)) {
+		return third;
+	}
+}
  }
