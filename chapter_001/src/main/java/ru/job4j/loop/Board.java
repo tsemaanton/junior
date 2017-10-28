1d72d7b1bd905ed45ef45cdb0a0d27747c4cdddd
tsemaanton <alsinghspb@gmail.com>
2017-10-28 06:55:56 +0300

    Task #13559

---
+++ chapter_001/src/main/java/ru/job4j/loop/Board.java
@@ -0,0 +1,32 @@
+package ru.job4j.loop;
+
+/**
+ * ....*class Board.
+ * ....*
+ * ....*@author Anton Tsema (alsinghspb@gmail.com)
+ * ....*@version 0.1
+ * ....*@since 27.10.2017
+ * .
+ */
+class Board {
+    /**
+     * Метод строит доску параметрами wight и hight.
+     *
+     * @param wigth  width
+     * @param height height
+     * @return vert
+     */
+    String paint(int wigth, int height) {
+        StringBuilder builder = new StringBuilder();
+        for (int i = 1; i <= wigth; i++) {
+            for (int j = 1; j <= height; j++) {
+                if ((i + j) % 2 == 0) {
+                    builder.append('X');
+                } else {
+                    builder.append(" ");
+                } }
+            builder.append("\r\n");
+        }
+        return builder.toString();
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/loop/BoardTest.java
@@ -0,0 +1,36 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+/**
+ ....*Test board.
+ ....*
+ ....*@author Anton Tsema (alsinghspb@gmail.com)
+ ....*@version 0.1
+ ....*@since 27.10.2017
+ .*/
+public class BoardTest {
+		/**
+	*Method addition.
+	*/
+    @Test
+    public void whenPaintBoardWithWidthThreeAndHeightThreeThenStringWithThreeColsAndThreeRows() {
+        Board board = new Board();
+        String result = board.paint(3, 3);
+        final String line = System.getProperty("line.separator");
+        String expected = String.format("X X%s X %sX X%s", line, line, line);
+        assertThat(result, is(expected));
+ }
+
+    @Test
+    public void whenPaintBoardWithWidthFiveAndHeightFourThenStringWithFiveColsAndFourRows() {
+        Board board = new Board();
+        String result = board.paint(4, 5);
+        final String line = System.getProperty("line.separator");
+        String expected = String.format("X X X%s X X %sX X X%s X X %s", line, line, line,line);
+        assertThat(result, is(expected));
+    }
+}
+
